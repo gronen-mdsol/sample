5144d21c610824ee4398b9acfa00f1cb5d19c8c3
Galit Ronen <Galit.Ronen@3ds.com>
2023-08-30 12:31:18 -0400

    Initial commit

---
+++ Sample/Sample/InterviewQuestions.swift
@@ -0,0 +1,71 @@
+//
+//  InterviewQuestions.swift
+//  Sample
+//
+//  Created by Galit RONEN on 8/30/23.
+//
+
+import Foundation
+
+class InterviewQuestions {
+    
+    func question1Permutations() {
+        let sampleString = "a dog crossed a street"
+        
+        // print result of  sampleString.permutationsByChar()
+        // print result of  sampleString.permutationsByWord()
+        
+        // Add some example tests
+
+    }
+    
+    func question4_Palindrome() {
+        let sampleString = "a dog crossed a street"
+        
+        // print result of  sampleString.isPalindrome()
+        
+        // Add some example tests
+    }
+    
+    func question3ArrayDuplicates() {
+        
+        let sampleArray = ["a", "dog", "crossed", "a", "street"]
+        
+        // print result of sampleArray.removeDuplicates()
+        
+        // Add some example tests
+
+    }
+    
+    func question4ReverseArray() {
+        // print result
+        
+        // Add some example tests
+    }
+}
+
+
+extension String {
+    func permutationsByChar() -> [String]? {
+        return nil
+    }
+    
+    func permutationsByWord() -> [String]? {
+        return nil
+    }
+    
+    func isPalindrome() -> Bool? {
+        return nil
+    }
+}
+
+
+extension Array {
+    func removeDuplicates() -> Self? {
+        return nil
+    }
+    
+    func reverse() -> Self? {
+        return nil
+    }
+}
